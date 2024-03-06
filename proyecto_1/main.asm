4f647084c2ab8f2217349cd45ea518b23262aa21
nliefaj <faj22060@uvg.edu.gt>
2024-03-05 20:28:21 -0600

    Intento de reloj
    Se intentó configurar las horas y minutos

--- proyecto_1/main.asm
+++ proyecto_1/main.asm
@@ -15,13 +15,21 @@
 //*******************************************************************
 .org 0x00
 	JMP MAIN			//Vector reset
-.org 0x08				//Vector interrupçion puerto c (entrada de los botones)
-	JMP ISR_PCINT1
+;.org 0x08				//Vector interrupçion puerto c (entrada de los botones)
+;	JMP ISR_PCINT1
+.org 0x0020 //interrupción timer0
+	JMP ISR_TIMER
 //*******************************************************************
 //CONTADORES
 //*******************************************************************
-.DEF count_mode=R18
-.DEF count_al_mode=R19
+;.DEF count_state=R18
+;.DEF count_al_mode=R19
+.DEF count_unidades=R21
+.DEF count_decenas=R22
+.DEF count_umin=R23
+.DEF count_dmin=R25
+.DEF counter=R18
+.DEF segundos=R19
 //*******************************************************************
 //STACK
 //*******************************************************************
@@ -43,7 +51,8 @@ MAIN:
 	LPM R16,Z
 
 SETUP:
-	LDI R16, 0b0010_0000
+	
+	LDI R16, 0b0000_0000//cambiar esto después de la prueba
 	OUT DDRC, R16 //Set PINES C segpun esquema de entradas y salidas
 
 	LDI r16,0b0001_1111 //habilitamos pullup para todos los botones del puerto C
@@ -61,19 +70,167 @@ SETUP:
 	LDI R16,0b0010//pines de evaluación de interrupción (PORTC)
 	STS PCICR,R16
 
+
+	//TIMER0
+	
+	LDI R16, 0b0000_0101//preescaler de 1024
+	STS TCCR0B,R16
+
+	LDI R16,178
+	STS TCNT0,R16
+
+	LDI R16,0b0000_0001
+	STS TIMSK0,R16
+	//PINES RX0 y RX1
+	LDI R16,0
+	STS UCSR0B,R16
+
+	CALL delayT0
+
 	SEI //habilitamos interrupcoines
 
-	CLR count_state
-	CLR count_al_mode
+	;CLR count_state
+	;CLR count_al_mode
+	CLR count_unidades
+	CLR count_decenas
+	CLR count_umin
+	CLR count_dmin
+	CLR counter
+	CLR segundos
+
 
 LOOP:
-	OUT PORTB, PB5
+	//enciende el display que tiene que mostrar y deja que los valores se muestren a tiempo completo
+	//bloque unidades de minuto
+	SBI PORTB,PB0
+	LDI ZH, HIGH(TABLA7SEG<<1)
+	LDI ZL, LOW(TABLA7SEG<<1)
+	ADD ZL,count_unidades
+	CLR R24
+	LPM R24,Z //Load from program memory R16
+	OUT PORTD,R24
+	CALL delaybounce
+	CBI PORTB,PB0
+	CALL delaybounce
+	CALL delaybounce
+
+	//bloque para decenas de minuto
+	SBI PORTB,PB1
+	LDI ZH, HIGH(TABLA7SEG<<1)
+	LDI ZL, LOW(TABLA7SEG<<1)
+	ADD ZL,count_decenas
+	CLR R24
+	LPM R24,Z //Load from program memory R16
+	OUT PORTD,R24
+	CALL delaybounce
+	CBI PORTB,PB1
+	CALL delaybounce
+	CALL delaybounce
+
+	//bloque para unidades de hora
+	SBI PORTB,PB2
+	LDI ZH, HIGH(TABLA7SEG<<1)
+	LDI ZL, LOW(TABLA7SEG<<1)
+	ADD ZL,count_umin
+	CLR R24
+	LPM R24,Z //Load from program memory R16
+	OUT PORTD,R24
+	CALL delaybounce
+	CBI PORTB,PB2
+	CALL delaybounce
+	CALL delaybounce
+
+	//bloque para decenas de hora
+	SBI PORTB,PB3
+	LDI ZH, HIGH(TABLA7SEG<<1)
+	LDI ZL, LOW(TABLA7SEG<<1)
+	ADD ZL,count_dmin
+	CLR R24
+	LPM R24,Z //Load from program memory R16
+	OUT PORTD,R24
+	CALL delaybounce
+	CBI PORTB,PB3
+	CALL delaybounce
+	CALL delaybounce
+
+	RJMP LOOP
 
 //*******************************************************************
 //SUBRUTINAS DE INTERRUPCIÓN
 //*******************************************************************
 
-ISR_PCINT1:
+delayT0:
+	LDI R16, (1 << CS02) | (1 << CS00)
+	OUT TCCR0B, R16
+
+	LDI R16,178
+	OUT TCNT0, R16
+
+	RET
+ISR_TIMER:
+	PUSH R16
+	IN R16,SREG
+	PUSH R16
+
+	//SBI PORTB,PB5
+
+	LDI R16,178
+	OUT TCNT0,R16
+
+	INC counter
+	CPI counter, 178
+	BRNE SALTO
+	CLR counter
+
+	INC count_unidades
+	CPI count_unidades, 0b0000_1010
+	BREQ overflow
+	RJMP SALTO
+
+SALTO:
+	POP R16
+	OUT SREG,R16
+	POP R16
+	RETI
+
+overflow:
+	LDI count_unidades,0b0000
+	INC count_decenas
+	CPI count_decenas, 0b0000_0110
+	BREQ overflow_decenas
+	RJMP SALTO
+
+overflow_decenas:
+	LDI count_decenas,0b0000_0000
+	INC count_umin
+	CPI count_umin,0b0000_0101
+	BREQ verificar_hora
+	CPI count_umin,0b0000_1010
+	BREQ overflow_umin
+	RJMP SALTO
+
+overflow_umin:
+	LDI count_umin,0b0000_0000
+	INC count_dmin
+	RJMP SALTO
+
+verificar_hora:
+	CPI count_dmin,2
+	BREQ ultima_hora
+	RJMP SALTO
+
+ultima_hora:
+	LDI count_umin,0b0000_0000
+	LDI count_dmin,0b0000_0000
+	RJMP SALTO
+
+delaybounce:
+	LDI r16,250//250 para notar el valor de muestreo en los display
+	delay:
+		DEC r16
+		BRNE delay
+	RET
+/*ISR_PCINT1:
 	PUSH R16
 	IN R16,SREG
 	PUSH R16
@@ -97,7 +254,8 @@ ISR_POP:
 	POP R16
 	OUT SREG,R16
 	POP R16
-	RETI
+	RETI*/
+
 //*******************************************************************
 //TABLA DE VALORES
 //*******************************************************************

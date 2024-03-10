1fea0728a4c492658aa0b8ce7bf5749ba117f922
nliefaj <faj22060@uvg.edu.gt>
2024-03-09 21:51:24 -0600

    Reloj funciona
    Se optimizó el reloj

--- proyecto_1/main.asm
+++ proyecto_1/main.asm
@@ -15,21 +15,25 @@
 //*******************************************************************
 .org 0x00
 	JMP MAIN			//Vector reset
-;.org 0x08				//Vector interrupçion puerto c (entrada de los botones)
-;	JMP ISR_PCINT1
+.org 0x08				//Vector interrupçion puerto c (entrada de los botones)
+	JMP ISR_PCINT1
 .org 0x0020 //interrupción timer0
 	JMP ISR_TIMER
 //*******************************************************************
 //CONTADORES
 //*******************************************************************
-;.DEF count_state=R18
-;.DEF count_al_mode=R19
+;.DEF al_flag=R14; no se puede utilizar
+;fecha evaluarla en el registro 15
+;registro 16 y 17 para usarlos como variables fáciles
+.DEF mode=R18
+.DEF counter=R19
+.DEF segundos=R20
 .DEF count_unidades=R21
 .DEF count_decenas=R22
-.DEF count_umin=R23
-.DEF count_dmin=R25
-.DEF counter=R18
-.DEF segundos=R19
+;.DEF count_umin=R23
+;registro 24 único para mostrar valores
+;.DEF count_dmin=R25
+
 //*******************************************************************
 //STACK
 //*******************************************************************
@@ -52,7 +56,7 @@ MAIN:
 
 SETUP:
 	
-	LDI R16, 0b0000_0000//cambiar esto después de la prueba
+	LDI R16, 0b0001_1111//cambiar esto después de la prueba
 	OUT DDRC, R16 //Set PINES C segpun esquema de entradas y salidas
 
 	LDI r16,0b0001_1111 //habilitamos pullup para todos los botones del puerto C
@@ -67,17 +71,17 @@ SETUP:
 	LDI R16, 0b0001_1111 //coloca la máscara a pines del puerto c con botón
 	STS PCMSK1, R16	
 
-	LDI R16,0b0010//pines de evaluación de interrupción (PORTC)
+	LDI R16,0b0000_0010//pines de evaluación de interrupción (PORTC)
 	STS PCICR,R16
 
 
 	//TIMER0
 	
 	LDI R16, 0b0000_0101//preescaler de 1024
-	out TCCR0B,R16
+	OUT TCCR0B,R16
 
-	LDI R16,178
-	out TCNT0,R16
+	LDI R16,1
+	OUT TCNT0,R16
 
 	LDI R16,0b0000_0001
 	STS TIMSK0,R16
@@ -93,8 +97,6 @@ SETUP:
 	;CLR count_al_mode
 	CLR count_unidades
 	CLR count_decenas
-	CLR count_umin
-	CLR count_dmin
 	CLR counter
 	CLR segundos
 
@@ -103,9 +105,11 @@ LOOP:
 	//enciende el display que tiene que mostrar y deja que los valores se muestren a tiempo completo
 	//bloque unidades de minuto
 	SBI PORTB,PB0
+	LDI R16,0b0000_1111
+	AND R16,count_unidades
 	LDI ZH, HIGH(TABLA7SEG<<1)
 	LDI ZL, LOW(TABLA7SEG<<1)
-	ADD ZL,count_unidades
+	ADD ZL,R16
 	CLR R24
 	LPM R24,Z //Load from program memory R16
 	OUT PORTD,R24
@@ -116,9 +120,11 @@ LOOP:
 
 	//bloque para decenas de minuto
 	SBI PORTB,PB1
+	LDI R16,0b0000_1111
+	AND R16,count_decenas
 	LDI ZH, HIGH(TABLA7SEG<<1)
 	LDI ZL, LOW(TABLA7SEG<<1)
-	ADD ZL,count_decenas
+	ADD ZL,R16
 	CLR R24
 	LPM R24,Z //Load from program memory R16
 	OUT PORTD,R24
@@ -129,9 +135,12 @@ LOOP:
 
 	//bloque para unidades de hora
 	SBI PORTB,PB2
+	LDI R16,0b1111_0000
+	AND R16,count_unidades
+	SWAP R16
 	LDI ZH, HIGH(TABLA7SEG<<1)
 	LDI ZL, LOW(TABLA7SEG<<1)
-	ADD ZL,count_umin
+	ADD ZL,R16
 	CLR R24
 	LPM R24,Z //Load from program memory R16
 	OUT PORTD,R24
@@ -142,9 +151,12 @@ LOOP:
 
 	//bloque para decenas de hora
 	SBI PORTB,PB3
+	LDI R16,0b1111_0000
+	AND R16,count_decenas
+	SWAP R16
 	LDI ZH, HIGH(TABLA7SEG<<1)
 	LDI ZL, LOW(TABLA7SEG<<1)
-	ADD ZL,count_dmin
+	ADD ZL,R16
 	CLR R24
 	LPM R24,Z //Load from program memory R16
 	OUT PORTD,R24
@@ -163,7 +175,7 @@ delayT0:
 	LDI R16, (1 << CS02) | (1 << CS00)
 	OUT TCCR0B, R16
 
-	LDI R16,178
+	LDI R16,1
 	OUT TCNT0, R16
 
 	RET
@@ -184,13 +196,15 @@ ISR_TIMER:
 
 	INC segundos
 	CPI segundos,60
-	BREQ empieza
+	RJMP empieza
 	RJMP SALTO
 
 empieza:
 	CLR segundos
 	INC count_unidades
-	CPI count_unidades, 0b0000_1010
+	LDI R16,0b0000_1111
+	AND R16,count_unidades
+	CPI R16, 0b0000_1010
 	BREQ overflow
 	RJMP SALTO
 
@@ -201,34 +215,51 @@ SALTO:
 	RETI
 
 overflow:
-	LDI count_unidades,0b0000
+	LDI R16,0b1111_0000
+	AND count_unidades,R16
 	INC count_decenas
-	CPI count_decenas, 0b0000_0110
+	LDI R16,0b0000_1111
+	AND R16,count_decenas
+	CPI R16, 0b0000_0110
 	BREQ overflow_decenas
 	RJMP SALTO
 
 overflow_decenas:
-	LDI count_decenas,0b0000_0000
-	INC count_umin
-	CPI count_umin,0b0000_0101
+	;trabajar con unidades de hora
+	LDI R16,0b1111_0000
+	AND count_decenas,R16;pomner en 0 las unidades de minutos
+	LDI R16,0b0000_1111
+	SWAP count_unidades
+	INC count_unidades
+	AND R16,count_unidades
+	SWAP count_unidades
+	CPI R16,0b0000_0100
 	BREQ verificar_hora
-	CPI count_umin,0b0000_1010
-	BREQ overflow_umin
+	CPI R16,0b0000_1010
+	BREQ overflow_uhora
 	RJMP SALTO
 
-overflow_umin:
-	LDI count_umin,0b0000_0000
-	INC count_dmin
+overflow_uhora:
+	;trabajar con decenas de hora
+	LDI R16,0b0000_1111
+	AND count_unidades,R16
+	SWAP count_decenas
+	INC count_decenas
+	AND R16,count_decenas
+	SWAP count_decenas
 	RJMP SALTO
 
 verificar_hora:
-	CPI count_dmin,2
+	LDI R16,0b1111_0000
+	AND R16,count_decenas
+	SWAP R16
+	CPI R16,0b0000_0010
 	BREQ ultima_hora
 	RJMP SALTO
 
 ultima_hora:
-	LDI count_umin,0b0000_0000
-	LDI count_dmin,0b0000_0000
+	LDI count_unidades,0b0000_0000
+	LDI count_decenas,0b0000_0000
 	RJMP SALTO
 
 delaybounce:
@@ -241,35 +272,44 @@ delaybounce:
 //*******************************************************************
 //INTERRUPCIÓN DE BOTONES
 //*******************************************************************
-/*ISR_PCINT1:
+ISR_PCINT1:
 	PUSH R16
 	IN R16,SREG
 	PUSH R16
 
-	SBRS count_state, 0 //PC0 igual a 1?
-	RJMP ESTADO0
-	RJMP ESTADO1
 
-ESTADO0:
-	NOP
-ESTADO1:
-	IN R16,PINC
-	SBRS R16,0 ;PC0=1
-	INC count_state
-	CPI count_state,3
-	BREQ OVERFLOW
-	SBRS count_state,0//estado=0001?
-	CPI count_state,0
-	BREQ ISR_POP
 
-OVERFLOW:
-	CLR count_state
+	LDI R16,0b0001_1111 
+	IN R17,PORTC
+	COM R17
+	AND R17,R16
+
 	RJMP ISR_POP
+
+/*INC_MODE:
+	INC mode
+	CPI mode,0b0100
+	BREQ overflow_mode
+	RJMP ISR_POP
+
+ESTADOx1:
+	SBRS mode,1
+	RJMP ESTADO_01 ;configurar hora
+	;RJMP ESTADO 11 ;configurar fecha
+	RJMP ISR_POP
+
+ESTADO_01:*/
+	
+overflow_mode:
+	CLR mode
+	RJMP ISR_POP
+
+	
 ISR_POP:
 	POP R16
 	OUT SREG,R16
 	POP R16
-	RETI*/
+	RETI
 
 //*******************************************************************
 //TABLA DE VALORES
